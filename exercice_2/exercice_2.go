2fc5d236fea0c73b9166e3df99dee129a4db6a45
yann-fk-21 <yann.fonkoue@facsciences-uy1.cm>
2025-02-02 14:38:53 +0100

    finished exercises

--- exercice_2/exercice_2.go
+++ exercice_2/exercice_2.go
@@ -41,3 +41,39 @@ func DfsRecursive(graph map[string][]string, node string, visited map[string]boo
 
 	fmt.Println(visited)
 }
+
+func BfsShortestPath(graph map[string][]string, start, goal string) ([]string, bool) {
+    if start == goal {
+        return []string{start}, true
+    }
+
+    
+    queue := [][]string{{start}}
+    visited := map[string]bool{start: true}
+
+    for len(queue) > 0 {
+        path := queue[0]
+        queue = queue[1:]
+
+        node := path[len(path)-1]
+
+        for _, neighbor := range graph[node] {
+            if !visited[neighbor] {
+               
+                newPath := append([]string{}, path...)
+                newPath = append(newPath, neighbor)
+
+                
+                if neighbor == goal {
+                    return newPath, true
+                }
+
+                
+                queue = append(queue, newPath)
+                visited[neighbor] = true
+            }
+        }
+    }
+
+    return nil, false
+}
--- exercice_2/exercice_2_test.go
+++ exercice_2/exercice_2_test.go
@@ -1,6 +1,10 @@
 package exercice2
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func TestBFS(t *testing.T) {
 	graph := map[string][]string{
@@ -11,7 +15,9 @@ func TestBFS(t *testing.T) {
 		"E": {"B", "F"},
 		"F": {"C", "E"},
 	}
+	fmt.Println("***Test BFS***")
 	Bfs(graph, "A")
+	fmt.Printf("\n\n")
 }
 
 func TestDFS(t *testing.T) {
@@ -25,6 +31,43 @@ func TestDFS(t *testing.T) {
 	}
 
 	visited := make(map[string]bool)
-	
+	fmt.Println("***Test DFS***")
 	DfsRecursive(graph, "A", visited)
+	fmt.Printf("\n\n")
+}
+
+
+func TestBfsShortestPath(t *testing.T) {
+    graph := map[string][]string{
+        "A": {"B", "C"},
+        "B": {"A", "D", "E"},
+        "C": {"A", "F"},
+        "D": {"B"},
+        "E": {"B", "F"},
+        "F": {"C", "E"},
+    }
+
+    tests := []struct {
+        start     string
+        goal      string
+        expected  []string
+        connected bool
+    }{
+        {"A", "F", []string{"A", "C", "F"}, true},
+        {"A", "D", []string{"A", "B", "D"}, true},
+        {"A", "E", []string{"A", "B", "E"}, true},
+        {"D", "F", []string{"D", "B", "E", "F"}, true},
+        {"A", "A", []string{"A"}, true},
+        {"A", "G", nil, false}, // "G" n'existe pas dans le graphe
+    }
+
+	fmt.Println("**Test ShortestPath***")
+    for _, test := range tests {
+        path, connected := BfsShortestPath(graph, test.start, test.goal)
+        fmt.Printf("Test de %s à %s: Chemin trouvé: %v, Connecté: %v\n", test.start, test.goal, path, connected)
+        if !reflect.DeepEqual(path, test.expected) || connected != test.connected {
+            t.Errorf("BfsShortestPath(%q, %q) = %v, %v; want %v, %v",
+                test.start, test.goal, path, connected, test.expected, test.connected)
+        }
+    }
 }
